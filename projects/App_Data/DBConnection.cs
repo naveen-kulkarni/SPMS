2b0db9cd76f40324d2a07cac1dc167ca9ac8f6f4
Naveen kulkarni <33716521+naveen-kulkarni@users.noreply.github.com>
2024-08-20 22:38:11 +0530

    Initial Commit

---
+++ projects/App_Data/DBConnection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+namespace Project
+{
+    public class DBConnection
+    {
+
+        public DBConnection()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+        public SqlConnection GetDBConnection()
+        {
+
+            SqlConnection conn = new SqlConnection("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=SPMS;Data Source=SHREEMONI-PC\\SQLEXPRESS");
+            return conn;
+        }
+    }
+}
